8bfd4099ec3e0e6afec47435fce2e22b8fda28b6
sertdfyguhi-git <gdkamtalan@gmail.com>
2021-07-11 09:20:13 +0800

    remove loadingMessage

--- cli/helper/printing.py
+++ cli/helper/printing.py
@@ -1,24 +1,3 @@
-from threading import Thread
-
-class loadingMessage:
-    def __init__(self) -> None:
-        self.con = False
-
-    def start(self, message):
-        def p():
-            bars = ['|', '/', '\\']
-            i = 0
-            while self.con:
-                print(f'{bars[i]} {message}', end='\r')
-                i += 1 if i != 2 else -2
-        self.con = True
-        Thread(target=p, daemon=True).start()
-    
-    def end(self, message):
-        self.con = False
-        print(message)
-
-
 def err(message):
     print(f'\u001b[31mERROR: {message}\u001b[0m')
     exit()
